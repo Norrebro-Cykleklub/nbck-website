5508b9b8a89d0d082dfa844aa24a392a3863641a
Mike Jensen <jensen.mike57@gmail.com>
2022-09-28 12:45:18 +0200

    update meta title

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,7 @@ import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `nbck-website`,
+    title: `Nørrebro Cykleklub`,
     siteUrl: `http://norrebrock.dk`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
