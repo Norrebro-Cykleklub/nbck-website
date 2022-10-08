b6479931e4c30656fca4e0b037f3274ca0ef594f
Mike Jensen <jensen.mike57@gmail.com>
2022-10-08 10:54:23 +0200

    make header respond to window changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useScrollIntoView from '../hooks/scroll-into-view';
+import useResizeObserver from '../hooks/use-resize-observer';
 import Link from './Link';
 import windowSafe from './utils/windowSafe';
 
@@ -9,13 +10,30 @@ interface HeaderProps {
 
 export default function Header({ images }: HeaderProps) {
   const scrollIntoView = useScrollIntoView();
+  const [windowSize, setObservedElement] = useResizeObserver({
+    throttleInterval: 500,
+  });
+  const [dynamicHeaderHeight, setDynamicHeaderHeight] = useState(
+    windowSafe?.innerHeight ?? 0,
+  );
+
+  useEffect(() => {
+    const bodyNode = windowSafe?.document.body;
+
+    if (!windowSize && bodyNode) {
+      setObservedElement(bodyNode);
+    }
+
+    if (windowSize) {
+      setDynamicHeaderHeight(windowSize.height);
+    }
+  }, [setObservedElement, windowSize]);
+
   const nbckLogo = images.find(img => img.name === 'Norrebro_logo');
+  const headerHeight = dynamicHeaderHeight ?? windowSafe?.innerHeight ?? 0;
 
   return (
-    <header
-      className="masthead"
-      style={{ height: windowSafe?.innerHeight ?? 0 }}
-    >
+    <header className="masthead" style={{ height: headerHeight }}>
       <div className="container">
         <div className="intro-text">
           <img
---
+++ src/hooks/use-resize-observer.ts
@@ -0,0 +1,73 @@
+import { useEffect, useMemo, useState, useRef } from 'react';
+import ResizeObserver from 'resize-observer-polyfill';
+import throttle from 'lodash/throttle';
+import { useCallback } from 'react';
+
+interface ResizeObserverSize {
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+}
+
+interface ResizeObserverProps {
+  /** Interval between function executions */
+  throttleInterval?: number;
+}
+
+export default function useResizeObserver(props: ResizeObserverProps) {
+  const [observedElement, setObservedElement] = useState<HTMLElement | null>(
+    null,
+  );
+  const [observedElementSize, _setObservedElementSize] =
+    useState<ResizeObserverSize | null>(null);
+
+  const throttled = useRef(
+    throttle(
+      (newSize: ResizeObserverSize): void => {
+        _setObservedElementSize(newSize);
+      },
+      props.throttleInterval ?? 0,
+      { trailing: true, leading: false },
+    ),
+  );
+
+  const setObservedElementSize = useCallback(
+    (newSize: ResizeObserverSize) => {
+      if (props.throttleInterval) {
+        throttled.current(newSize);
+        return;
+      }
+
+      _setObservedElementSize(newSize);
+    },
+    [props.throttleInterval],
+  );
+
+  const ro = useMemo(() => {
+    return new ResizeObserver((entries: ResizeObserverEntry[]) => {
+      for (const entry of entries) {
+        const { left, top, width, height } = entry.contentRect;
+        setObservedElementSize({
+          left,
+          top,
+          width,
+          height,
+        });
+      }
+    });
+  }, [setObservedElementSize]);
+
+  useEffect(() => {
+    if (!observedElement) {
+      return;
+    }
+
+    ro.observe(observedElement);
+    return () => {
+      ro.unobserve(observedElement);
+    };
+  }, [observedElement, ro]);
+
+  return [observedElementSize, setObservedElement] as const;
+}
