f843863057e3efb425c26e859baa8b142f26aa30
Mike Jensen <jensen.mike57@gmail.com>
2022-10-08 14:25:12 +0200

    create contact form

---
+++ lambda/contactForm.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-underscore-dangle */
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+exports.handler = async function sendGridEmail(event, context, callback) {
+  const clientIP = (event.headers || {})['x-nf-client-connection-ip'];
+  const { name, email, phone, message } = JSON.parse(event.body);
+
+  const html = `<div><strong>Ny besked via Nørrebro CK's kontaktformular</strong><p>Navn: ${name}</p><p>Mail: ${email}</p><p>Telefon: ${phone}</p><p>Besked: ${message}</p><p>Afsenders IP addresse: ${clientIP}</p></div>`;
+
+  const msg = {
+    to: ['torefolkerman@gmail.com', 'tobiasjohansorensen@gmail.com'],
+    from: 'noreply@norrebrock.dk',
+    subject: 'Nørrebro cykleklub - Kontakt',
+    html,
+  };
+
+  await sgMail
+    .send(msg)
+    .then(() => {
+      callback(null, {
+        statusCode: 200,
+      });
+    })
+    .catch(error => {
+      callback(error);
+    });
+};
--- src/components/Dialogs/ContactForm.tsx
+++ src/components/Dialogs/ContactForm.tsx
@@ -1,75 +1,186 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import axios from 'axios';
 import { useLayoutContext } from '../Layout/Context';
 import Dialog from './Dialog';
+import windowSafe from '../utils/windowSafe';
 
 export default function ContactFormDialog() {
   const { contactForm } = useLayoutContext();
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [message, setMessage] = useState('');
+
+  const clear = useCallback(() => {
+    setSuccess(false);
+    setError(false);
+    setName('');
+    setEmail('');
+    setPhone('');
+    setMessage('');
+  }, []);
+
+  const onClose = useCallback(() => {
+    contactForm.hide();
+    clear();
+  }, [clear, contactForm]);
+
+  const submit = useCallback(async () => {
+    await axios
+      .post(windowSafe?.location.origin + '/.netlify/functions/contactForm', {
+        name,
+        email,
+        phone,
+        message,
+      })
+      .then(() => {
+        setSuccess(true);
+      })
+      .catch(err => {
+        const data = err?.response?.data;
+        console.error(data?.errorMessage);
+        setError(true);
+      });
+  }, [email, message, name, phone]);
+
+  const SuccessDialogContent = useCallback(() => {
+    return (
+      <div id="nbroSlimModalBody" className="modal-body">
+        <h2 className="breakword">Besked afsendt</h2>
+        <p>Tak for din henvendelse. Vi har modtaget din besked.</p>
+        <button
+          className="btn btn-primary"
+          data-dismiss="modal"
+          type="button"
+          onClick={() => {
+            contactForm.hide();
+            clear();
+          }}
+        >
+          <i className="fa fa-times" /> Tilbage
+        </button>
+      </div>
+    );
+  }, [clear, contactForm]);
+
+  const ErrorDialogContent = useCallback(() => {
+    return (
+      <div id="nbroSlimModalBody" className="modal-body">
+        <h2 className="breakword">Beskeden kan ikke sendes</h2>
+        <p>
+          Beskeden kan ikke sendes da et eller flere af felterne ikke er udfyldt
+          korrekt. Prøv venligst igen.
+        </p>
+        <button
+          className="btn btn-primary"
+          data-dismiss="modal"
+          type="button"
+          onClick={() => {
+            setError(false);
+          }}
+        >
+          <i className="fa fa-times" /> Tilbage
+        </button>
+      </div>
+    );
+  }, []);
+
+  const canSubmit = [name, email, phone, message].every(Boolean);
 
   return (
-    <Dialog open={contactForm.visible} onClose={contactForm.hide}>
-      {({ closeButton }) => (
-        <>
-          <div className="row">
-            <div className="col-lg-12 text-center">
-              <h2 className="breakword">Kontakt os</h2>
-              <p className="item-intro text-muted">
-                Hvis du ønsker at vide mere, så send os en besked.
-              </p>
-            </div>
-          </div>
-          <div className="row" id="kontaktModal">
-            <div className="col-lg-12">
+    <Dialog open={contactForm.visible} onClose={onClose}>
+      {useCallback(
+        ({ closeButton }) => {
+          if (success) {
+            return <SuccessDialogContent />;
+          }
+
+          if (error) {
+            return <ErrorDialogContent />;
+          }
+
+          return (
+            <>
               <div className="row">
-                <div id="tokenKontakt">
-                  <input
-                    name="__RequestVerificationToken"
-                    type="hidden"
-                    value="gwcWldOOJdqufn3Sy7bPq6G8enTK_98UuSIfuXJLJ24JSBA_PI7aYN-n41r70kZfJ-1v598QlwE8ZQCnv2jwjbsgrxBRSboVM97BdrPBSgw1"
-                  />
-                </div>
-                <div className="col-md-6">
-                  <div className="kontaktGroup">
-                    <input
-                      className="kontaktControl"
-                      id="nameKontakt"
-                      placeholder="Dit navn *"
-                    />
-                  </div>
-                  <div className="kontaktGroup">
-                    <input
-                      className="kontaktControl"
-                      id="emailKontakt"
-                      placeholder="Din email *"
-                    />
-                  </div>
-                  <div className="kontaktGroup">
-                    <input
-                      className="kontaktControl"
-                      id="phoneKontakt"
-                      placeholder="Dit telefonnummer *"
-                    />
-                  </div>
-                </div>
-                <div className="col-md-6">
-                  <div className="kontaktGroup">
-                    <textarea
-                      className="kontaktControl"
-                      id="messageKontakt"
-                      placeholder="Din besked *"
-                    ></textarea>
-                  </div>
-                </div>
-                <div className="clearfix"></div>
                 <div className="col-lg-12 text-center">
-                  <button id="sendMessageButton" className="btn btn-xl">
-                    Send besked
-                  </button>
+                  <h2 className="breakword">Kontakt os</h2>
+                  <p className="item-intro text-muted">
+                    Hvis du ønsker at vide mere, så send os en besked.
+                  </p>
                 </div>
               </div>
-            </div>
-          </div>
-          {closeButton}
-        </>
+              <div className="row" id="kontaktModal">
+                <div className="col-lg-12">
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="kontaktGroup">
+                        <input
+                          className="kontaktControl"
+                          placeholder="Dit navn *"
+                          value={name}
+                          onChange={e => setName(e.target.value)}
+                        />
+                      </div>
+                      <div className="kontaktGroup">
+                        <input
+                          className="kontaktControl"
+                          placeholder="Din email *"
+                          value={email}
+                          onChange={e => setEmail(e.target.value)}
+                        />
+                      </div>
+                      <div className="kontaktGroup">
+                        <input
+                          className="kontaktControl"
+                          placeholder="Dit telefonnummer *"
+                          value={phone}
+                          onChange={e => setPhone(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="kontaktGroup">
+                        <textarea
+                          className="kontaktControl"
+                          placeholder="Din besked *"
+                          value={message}
+                          onChange={e => setMessage(e.target.value)}
+                        ></textarea>
+                      </div>
+                    </div>
+                    <div className="clearfix"></div>
+                    <div className="col-lg-12 text-center">
+                      <button
+                        className={
+                          canSubmit ? 'btn btn-xl' : 'btn btn-xl disabled'
+                        }
+                        onClick={submit}
+                        disabled={!canSubmit}
+                      >
+                        Send besked
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {closeButton}
+            </>
+          );
+        },
+        [
+          ErrorDialogContent,
+          SuccessDialogContent,
+          canSubmit,
+          email,
+          error,
+          message,
+          name,
+          phone,
+          submit,
+          success,
+        ],
       )}
     </Dialog>
   );
